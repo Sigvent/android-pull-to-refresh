feb1472e42a4e457f1d9d56949e3c750d18c43e5
Sigvent <michaelduong00@hotmail.com>
2016-10-10 16:19:32 +0200

    Update PullToRefreshWebView.java
    remove FloatMath library

--- library/src/main/java/com/handmark/pulltorefresh/library/PullToRefreshWebView.java
+++ library/src/main/java/com/handmark/pulltorefresh/library/PullToRefreshWebView.java
@@ -24,7 +24,6 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.util.FloatMath;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
@@ -161,7 +160,7 @@ public class PullToRefreshWebView extends PullToRefreshBase<WebView> {
 		}
 
 		private int getScrollRange() {
-			return (int) Math.max(0, FloatMath.floor(mRefreshableView.getContentHeight() * mRefreshableView.getScale())
+			return (int) Math.max(0, Math.floor(mRefreshableView.getContentHeight() * mRefreshableView.getScale())
 					- (getHeight() - getPaddingBottom() - getPaddingTop()));
 		}
 	}
